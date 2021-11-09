1f9e822eeb179e1e0ab2c41983419e7bbc2e0951
Ben Mills <fuzzyllama@gmail.com>
2021-11-08 18:07:45 -0800

    Adds a book route and working login button

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,6 +11,20 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
 
+  componentDidMount() {
+    this.getBooks()
+  }
+
+  getBooks = async () => {
+    let apiUrl = `${process.env.REACT_APP_DB_URL}/books`;
+    try {
+      const response = await axios.get(apiUrl);
+      this.setState({books: response.data})
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
 
     /* TODO: render user's books in a Carousel */
@@ -18,7 +33,7 @@ class BestBooks extends React.Component {
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
+        {this.state.books.length > 0 ? (
           <p>Book Carousel coming soon</p>
         ) : (
           <h3>No Books Found :(</h3>
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,11 +1,36 @@
-import { Component } from "react";
+import {Component} from "react";
 
 class LoginForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      email: '',
+    }
+  }
+
+  handleClick = () => {
+    this.props.loginHandler(this.state.user, this.state.email);
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({user: e.target.value})
+
+  }
 
   render() {
     /* TODO: create a simple login form that collects username and and email, and lets parent component know when form has been submitted */
     return (
-      <p>LoginForm Coming Soon</p>
+      <>
+        <p>LoginForm Coming Soon</p>
+        <>
+          <input type='text' onChange={this.handleChange} placeholder="enter username" />
+          <input type='text' onChange={this.handleChange} placeholder="enter email" />
+          <button onClick={this.handleClick}>Submit</button>
+        </>
+      </>
     );
   }
 };
